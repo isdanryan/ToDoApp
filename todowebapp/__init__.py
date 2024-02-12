b030283c3ff434519d1d26c67f6ef6eb9178d227
isdanryan <isdanryan@gmail.com>
2024-02-12 22:44:08 +0000

    Import routes from views
    Forgot to import previously defined route from last commit

--- todowebapp/__init__.py
+++ todowebapp/__init__.py
@@ -2,12 +2,16 @@ from dotenv import load_dotenv
 import os
 from flask import Flask
 
-#load enviroment variables
+# load enviroment variables
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = SECRET_KEY
+
+    # import routes
+    from .views import views
+    app.register_blueprint(views, url_prefix='/')
 
     return app
