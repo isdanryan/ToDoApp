3ee352acb6957f5e3ba18686d9ebc66bc56604df
isdanryan <isdanryan@gmail.com>
2024-02-13 23:28:33 +0000

    Update view.py
    Add signup route

--- todowebapp/views.py
+++ todowebapp/views.py
@@ -6,3 +6,8 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template('todos.html')
+
+@views.route('/signup')
+def signup():
+     # register user details and add to database
+     return render_template('signup.html')
